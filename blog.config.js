e420a646dafcf9da50e0217bd18659325e3e0cdc
mike838383 <67719567+mike838383@users.noreply.github.com>
2022-07-19 09:19:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: 'Mike', // 作者
   BIO: '一个普通的打工人', // 作者简介
-  LINK: 'https://bilibili.com', // 网站地址
+  LINK: 'https://mike-blog.vercel.app/', // 网站地址
   KEYWORDS: 'Notion,软件，博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
